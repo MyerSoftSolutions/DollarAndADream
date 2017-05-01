8c8ba0ea505f1560a3a8f8dc7176f1ab7baa22c5
Joel Myers <myersoftsolutions@gmail.com>
2017-05-01 15:11:28 -0400

    Home to SendView navigation added

--- DollarAndADream/DollarAndADream/DDSendViewController.swift
+++ DollarAndADream/DollarAndADream/DDSendViewController.swift
@@ -12,6 +12,7 @@ class DDSendViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createCustomBackButton("X")
 
         // Do any additional setup after loading the view.
     }
