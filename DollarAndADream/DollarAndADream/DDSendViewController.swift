253ad4047991f61bd8400196a661a94387bd53cc
Joel Myers <myersoftsolutions@gmail.com>
2017-05-10 16:42:34 -0400

    SendMoneyVC Look and Feel additions

--- DollarAndADream/DollarAndADream/DDSendViewController.swift
+++ DollarAndADream/DollarAndADream/DDSendViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class DDSendViewController: UIViewController {
+    
+    var amount = 0.89
 
+    @IBOutlet var amountSlider: UISlider!
+    @IBOutlet var sendBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.createCustomBackButton("X")
+        self.amountSlider.minimumValue = 0.01
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +27,25 @@ class DDSendViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        
+        self.amount = Double(sender.value)
+        if self.amount != 1.00 {
+            let amt = Int(self.amount * 100)
+            self.sendBtn.setTitle("SEND \(String(format: "%d", amt))¢", for: .normal)
+            if self.sendBtn.currentTitle == "SEND 0¢" {
+                self.sendBtn.setTitle("SEND 1¢", for: .normal)
+                self.sendBtn.isEnabled = false
+            } else {
+                self.sendBtn.isEnabled = true
+
+            }
+        } else {
+            self.sendBtn.setTitle("SEND $\(String(format: "%.2f", self.amount))", for: .normal)
+        }
+        
+        
+    }
 
     /*
     // MARK: - Navigation
