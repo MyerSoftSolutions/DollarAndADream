e85fa7364850ef594c151b427f77b13869e81f1a
Joel Myers <myersoftsolutions@gmail.com>
2017-06-16 03:45:09 -0400

    Settings Receiver Type Look Picker View to select Receiver type

---
+++ DollarAndADream/DollarAndADream/DDRecTypeSelectionViewController.swift
@@ -0,0 +1,120 @@
+//
+//  DDRecTypeSelectionViewController.swift
+//  DollarAndADream
+//
+//  Created by Joel Myers on 6/16/17.
+//  Copyright © 2017 Dollar & a Dream LLC. All rights reserved.
+//
+
+import UIKit
+
+
+
+protocol DDRecTypePickerDelegate {
+    func typeSelected(_ rowPressed: Int)
+}
+
+class DDRecTypeSelectionViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    var forGraphVal : Int?
+    
+    var arr = ["Education", "Person in Need", "Entrepreneur"]
+    var delegate : DDRecTypePickerDelegate?
+    var selectedIdx : Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.pickerView.selectRow(selectedIdx!, inComponent: 0, animated: true)
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return arr.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arr[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.delegate?.typeSelected(row)
+        
+    }
+    
+}
+
+protocol RecTypeSelectionModalDelegate {
+    func selectMetric(_ idx: Int)
+    func nixModal()
+}
+
+class RecTypeSelectionModalController : UIViewController, DDRecTypePickerDelegate, UIGestureRecognizerDelegate{
+    
+    var pickerView : UIView?
+    var pickerViewController : DDRecTypeSelectionViewController?
+    var modalDelegate : RecTypeSelectionModalDelegate?
+    var forGraphVal : Int?
+    var selectedIdx : Int?
+    var arr : [String]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .overCurrentContext
+        self.view.backgroundColor = UIColor.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.4)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(RecTypeSelectionModalController.dismissModal))
+        tap.delegate = self
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        pickerViewController = DDRecTypeSelectionViewController(nibName: "DDRecTypeSelectionViewController", bundle: nil)
+        if arr != nil {
+            pickerViewController?.arr = self.arr!
+        }
+        pickerViewController?.forGraphVal = self.forGraphVal
+        pickerViewController?.selectedIdx = selectedIdx
+        pickerViewController?.delegate = self
+        self.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+        
+    }
+    
+    func typeSelected(_ rowPressed: Int) {
+        modalDelegate?.selectMetric(rowPressed)
+    }
+
+    
+    func dismissModal() {
+        self.modalDelegate?.nixModal()
+    }
+    //
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        pickerView = pickerViewController?.view
+        //        metricPickerView?.layer.cornerRadius = 7.0
+        
+        
+        pickerView?.frame = CGRect(x: 17.0, y: 40.0, width: UIScreen.main.bounds.size.width, height: 215)
+        
+        pickerView!.center = self.view.center
+        
+        self.view.addSubview(pickerView!)
+        self.addChildViewController(pickerViewController!)
+        pickerViewController?.didMove(toParentViewController: self)
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
--- DollarAndADream/DollarAndADream/DDSignUpViewController.swift
+++ DollarAndADream/DollarAndADream/DDSignUpViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DDSignUpViewController: UIViewController, UIScrollViewDelegate {
+class DDSignUpViewController: UIViewController, UIScrollViewDelegate, RecTypeSelectionModalDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var accountTypeSegControl: UISegmentedControl!
@@ -16,12 +16,13 @@ class DDSignUpViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet var recTypeLabelHeightCon: NSLayoutConstraint!
     @IBOutlet var recTypeButtonHeightCon: NSLayoutConstraint!
     @IBOutlet weak var recTypeView: UIView!
+    @IBOutlet weak var recTypeBtn: UIButton!
+    var arr = ["Education", "Person in Need", "Entrepreneur"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.createCustomBackButton("X")
         self.accountTypeSegControl.selectedSegmentIndex = 0
-//        self.recTypeLabelHeightCon.constant = 0
-//        self.recTypeButtonHeightCon.constant = 0
         self.segBottomToSeparatorCon.constant = 10
         
         self.scrollView.keyboardDismissMode = .interactive
@@ -32,18 +33,48 @@ class DDSignUpViewController: UIViewController, UIScrollViewDelegate {
         if sender.selectedSegmentIndex == 0 {
             recTypeView.isHidden = true
             self.accountTypeSegControl.selectedSegmentIndex = 0
-//            self.recTypeLabelHeightCon.constant = 0
-//            self.recTypeButtonHeightCon.constant = 0
             self.segBottomToSeparatorCon.constant = 10
         }  else {
             recTypeView.isHidden = false
             self.accountTypeSegControl.selectedSegmentIndex = 1
-//            self.recTypeLabelHeightCon.constant = 22
-//            self.recTypeButtonHeightCon.constant = 30
             self.segBottomToSeparatorCon.constant = 88
         }
         
         
+    }
+    @IBAction func recTypePressed(_ sender: UIButton) {
+        
+        if let pickerModalView = UIApplication.shared.keyWindow!.viewWithTag(2000) {
+            pickerModalView.removeFromSuperview()
+        }
+        
+        let pickerModal = RecTypeSelectionModalController()
+        pickerModal.arr = ["Education", "Person in Need", "Entrepreneur"]
+        
+        pickerModal.modalDelegate = self
+        pickerModal.selectedIdx = 0
+        
+        pickerModal.view.tag = 2000
+        
+        UIApplication.shared.keyWindow!.addSubview(pickerModal.view)
+        self.addChildViewController(pickerModal)
+        pickerModal.didMove(toParentViewController: self)
+    }
+    
+    
+    //MARK: RecTypePickerModalDelegate Methods
+    func selectMetric(_ idx: Int) {
+        recTypeBtn.setTitle(arr[idx], for: .normal)
+        if let invalidView = UIApplication.shared.keyWindow!.viewWithTag(2000) {
+            invalidView.removeFromSuperview()
+        }
+
+    }
+    
+    func nixModal() {
+        if let invalidView = UIApplication.shared.keyWindow!.viewWithTag(2000) {
+            invalidView.removeFromSuperview()
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
